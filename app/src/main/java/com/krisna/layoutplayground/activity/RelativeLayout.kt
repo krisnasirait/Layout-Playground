812da0f946693bbdfed78b5ed4d9cd2d20d1ed0a
Krisna Sirait <krisnapande00@outlook.com>
2022-07-14 11:56:20 +0700

    added layout in activity_relative_layout.xml

--- app/src/main/java/com/krisna/layoutplayground/activity/RelativeLayout.kt
+++ app/src/main/java/com/krisna/layoutplayground/activity/RelativeLayout.kt
@@ -1,12 +1,20 @@
 package com.krisna.layoutplayground.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.krisna.layoutplayground.R
 
 class RelativeLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_layout)
+
+        val clickLanjut = findViewById<Button>(R.id.btn_lanjut)
+        clickLanjut.setOnClickListener{
+            val intent = Intent(this, ConstraintLayout::class.java)
+            startActivity(intent)
+        }
     }
 }
