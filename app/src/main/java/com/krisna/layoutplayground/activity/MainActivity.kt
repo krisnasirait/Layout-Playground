de785d81ef9753f9750ae1856db96bdaf84d470e
Krisna Sirait <krisnapande00@outlook.com>
2022-07-14 10:31:34 +0700

    Initial commit

---
+++ app/src/main/java/com/krisna/layoutplayground/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.krisna.layoutplayground.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.krisna.layoutplayground.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
